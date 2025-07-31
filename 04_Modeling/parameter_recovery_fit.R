7b4a6d5e541208281ba4e15781d695e77ec86df7
Oberbauer <bbc6695@uni-hamburg.de>
2025-07-31 08:09:41 +0200

    Change index of data set fitted

--- 04_Modeling/parameter_recovery_fit.R
+++ 04_Modeling/parameter_recovery_fit.R
@@ -117,7 +117,7 @@ fixProps <- fixProps/rowSums(fixProps)
 # Estimate Parameters for Simulated Data ------
 
 # recover parameters for the data set simulated using the parameter set with rank likelihood (1-10)
-rank_likelihood <- 8 # 1 = most likely parameter set was used for simulating the data
+rank_likelihood <- 1 # 1 = most likely parameter set was used for simulating the data
 
 ### Put data in list ------
 
