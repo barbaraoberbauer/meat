bc4e9b62eb8a998396b770bb5625f99f87bd7a80
Oberbauer <bbc6695@uni-hamburg.de>
2025-08-01 08:55:27 +0200

    Change scheme and border size of bars

--- functions/fun_plot_change_parameters.R
+++ functions/fun_plot_change_parameters.R
@@ -10,15 +10,18 @@
 plot_change_param <- function(combined_mcmcfin, parameter, bins, x_title){
   
   ggplot(combined_mcmcfin, aes(parameter)) +
-    geom_histogram(color = scales::alpha("black", 0.5), 
+    geom_histogram(color = scales::alpha("black", 1), 
+                   size = 1.2,
                    fill = "darkgrey", 
                    bins = bins) +
     geom_vline(xintercept = 0, color = "#CB181D", linetype = "dashed", linewidth = 1.2) +
     geom_vline(xintercept = quantile(parameter, c(.025,.975)), color = "black", linetype = "dashed", linewidth = 1.2) +
     labs(x = x_title, y = "", title = '') + 
     #coord_cartesian(ylim = c(0, 15000)) +
-    theme_classic() +
-    theme(axis.text.x=element_text(size=13),
+    theme_bw() +
+    theme(panel.grid.major = element_blank(),   
+          panel.grid.minor = element_blank(),
+          axis.text.x=element_text(size=13),
           axis.text.y=element_blank(),
           axis.ticks.y=element_blank(),
           plot.margin = margin(t = 17,
--- functions/fun_plot_posterior_distributions.R
+++ functions/fun_plot_posterior_distributions.R
@@ -12,18 +12,22 @@ plot_posterior_dist <- function(combined_mcmcfin, parameter1, parameter2, col_pa
     cols <- c("1" = scales::alpha(col_param1, 0.7), "2" = scales::alpha(col_param2, 0.6))
   
   ggplot(combined_mcmcfin, aes(parameter1)) +
-    geom_histogram(color = "black", 
+    geom_histogram(color = "black",
+                   size = 1.2,
                    aes(fill = "1"), 
                    bins = bins) +
     geom_histogram(aes(parameter2, fill = "2"), 
                    data = combined_mcmcfin,
                    color = "black", 
+                   size = 1.2,
                    bins = bins) +
     scale_fill_manual(name = "Session", values = cols) +
     labs(x = x_title, y = "Posterior Density", title = '') + 
     #coord_cartesian(ylim = c(0, 15000)) +
-    theme_classic() +
-    theme(axis.text.x=element_text(size=12),
+    theme_bw() +
+    theme(panel.grid.major = element_blank(),   
+          panel.grid.minor = element_blank(),
+          axis.text.x=element_text(size=12),
           axis.text.y=element_blank(),
           axis.ticks.y=element_blank(),
           plot.margin = margin(t = 17,
