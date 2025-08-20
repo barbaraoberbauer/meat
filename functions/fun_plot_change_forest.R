ab192cdcc4da4098047a4d8eb153559ddb97452f
Oberbauer <bbc6695@uni-hamburg.de>
2025-08-20 09:27:29 +0200

    Add y axis ticks again

--- functions/fun_plot_change_forest.R
+++ functions/fun_plot_change_forest.R
@@ -26,7 +26,7 @@ plot_change_param_forest <- function(data, param, xtitle, cols){
           axis.title.y = element_blank(),
           axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0), size = 14),
           axis.text.y = element_blank(),
-          axis.ticks = element_blank(),
+          axis.ticks.y = element_blank(),
           plot.margin = margin(t = 10,
                                r = 10,
                                b = 10,
