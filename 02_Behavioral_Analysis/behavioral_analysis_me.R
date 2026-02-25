45b18a8bdaa9f17d9d411845ef946f66191c9ea9
Barbara Oberbauer <barbara.oberbauer@uni-hamburg.de>
2026-02-25 10:23:40 +0100

    Finalize adaptation of replication data

--- 02_Behavioral_Analysis/behavioral_analysis_me.R
+++ 02_Behavioral_Analysis/behavioral_analysis_me.R
@@ -147,205 +147,359 @@ fixedEffectsDirectReplication <- fixed_effects_function(dfReplication %>%
                                                                    consumption_translation == "emission_add" |
                                                                    consumption_translation == "rating_add"))
 
-# TODO - CONTINUE
-
 ### Set up model ----
 
-# Effect of translation of energy and water consumption (no translation/control) on product choice  
-contrasts(df$consumption_translation) <- 
-  contr.treatment(levels(df$consumption_translation), base = 1)
+choice_model_function <- function(data){
+  
+  # Effect of translation of energy and water consumption (no translation/control) on product choice  
+  contrasts(data$consumption_translation) <- 
+    contr.treatment(levels(data$consumption_translation), base = 1)
+  
+  choice_model <- glmer(choice ~ (session | id) + (1 | task) + session * consumption_translation, 
+                        data = data, 
+                        family = binomial(link = "logit"), 
+                        control = glmerControl(optimizer="bobyqa", 
+                                               optCtrl = list(maxfun=2e5)))
+  
+  return(choice_model)
+  
+}
 
-choice_model <- glmer(choice ~ (session | id) + (1 | task) + session * consumption_translation, 
-                      data = df, 
-                      family = binomial(link = "logit"), 
-                      control = glmerControl(optimizer="bobyqa", 
-                                             optCtrl = list(maxfun=2e5)))
+choiceModelOriginal <- choice_model_function(dfOriginal)
+choiceModelReplication <- choice_model_function(dfReplication)
 
-summary(choice_model)
+summary(choiceModelOriginal)
+summary(choiceModelReplication)
 
 
 ### EMM Comparison -------
 
-EMM <- emmeans(choice_model, 
-               ~ consumption_translation * session, 
-               type = "response")
+emm_function <- function(choice_model){
+  
+  EMM <- emmeans(choice_model, 
+                 ~ consumption_translation * session, 
+                 type = "response")
+  
+  emm_session <- pairs(EMM, reverse = TRUE, simple = "session")
+  
+  emm_session_confint <- confint(emm_session)
+  
+  return(list(
+    emm_session = emm_session,
+    emm_session_confint = emm_session_confint
+  ))
+  
+}
 
-emm_session <- pairs(EMM, reverse = TRUE, simple = "session")
-
-emm_session_confint <- confint(emm_session)
-
+emmOriginal <- emm_function(choiceModelOriginal)
+emmReplication <- emm_function(choiceModelReplication)
 
 
 # Attention -----
 
 ### Descriptives  ---------
 
-# aggregate data on subject level
-att_prob_id <- df %>%
-  group_by(session, consumption_translation, id) %>%
-  summarise(mean_diff_dt_id = mean(diff_t_options, na.rm = TRUE), 
-            se_diff_dt_id = sd(diff_t_options, na.rm = TRUE)/sqrt(sum(!is.na(diff_t_options)))
-  ) %>%
-  arrange(consumption_translation)
+att_descriptives_function <- function(data){
+  
+  # aggregate data on subject level
+  att_prob_id <- data %>%
+    group_by(session, consumption_translation, id) %>%
+    summarise(mean_diff_dt_id = mean(diff_t_options, na.rm = TRUE), 
+              se_diff_dt_id = sd(diff_t_options, na.rm = TRUE)/sqrt(sum(!is.na(diff_t_options)))
+    ) %>%
+    arrange(consumption_translation)
+  
+  # aggregate data on group level
+  att_prob_group <- att_prob_id %>%
+    group_by(session, consumption_translation) %>%
+    summarise(mean_diff_dt = mean(mean_diff_dt_id, na.rm = TRUE), 
+              se_diff_dt = sd(mean_diff_dt_id, na.rm = TRUE)/sqrt(sum(!is.na(mean_diff_dt_id)))
+    ) %>%
+    arrange(consumption_translation)
+  
+  return(list(
+    att_id_level = att_prob_id,
+    att_group_level = att_prob_group
+  ))
+  
+}
 
-# aggregate data on group level
-att_prob_group <- att_prob_id %>%
-  group_by(session, consumption_translation) %>%
-  summarise(mean_diff_dt = mean(mean_diff_dt_id, na.rm = TRUE), 
-            se_diff_dt = sd(mean_diff_dt_id, na.rm = TRUE)/sqrt(sum(!is.na(mean_diff_dt_id)))
-  ) %>%
-  arrange(consumption_translation)
+attDescriptivesOriginal <- att_descriptives_function(dfOriginal)
+attDescriptivesReplication <- att_descriptives_function(dfReplication)
+
 
 ### Attention Consistency in Session 1 between Consumption Translations ------
 
-# Fit a model without the session interaction
-att_model_session1 <- lmer(diff_t_options ~ consumption_translation + (1 | id) + (1 | task), 
-                               data = df_session1, 
-                               REML = FALSE,
-                               control = lmerControl(optimizer="bobyqa", 
-                                                     optCtrl = list(maxfun=2e5)))
+baseline_att <- function(data){
+  
+  # Subset the data for session 1
+  data_session1 <- subset(data, session == 1)
+  
+  # Fit a model without the session interaction
+  att_model_session1 <- lmer(diff_t_options ~ consumption_translation + (1 | id) + (1 | task), 
+                             data = data_session1, 
+                             REML = FALSE,
+                             control = lmerControl(optimizer="bobyqa", 
+                                                   optCtrl = list(maxfun=2e5)))
+  
+  return(att_model_session1)
+  
+}
 
-# Perform an ANOVA to see if condition has a significant effect
-Anova(att_model_session1)
+baselineAttOriginal <- baseline_att(dfOriginal)
+baselineAttReplication <- baseline_att(dfReplication)
+
+# Perform an ANOVA to see if consumption translation has a significant effect
+Anova(baselineAttOriginal)
+Anova(baselineAttReplication)
 
 
 ### Check for Significant Fixed Effects -------
-fixed_effects_att <- afex::mixed(diff_t_options ~ (1 | id) + (1 | task) + session * consumption_translation, 
-                             data = df, 
-                             REML = FALSE, 
-                             control = lmerControl(optimizer="bobyqa", 
-                                                    optCtrl = list(maxfun=2e5)),
-                             method = 'LRT')
 
+fixed_effects_att_function <- function(data){
+  
+  fixed_effects_att <- afex::mixed(diff_t_options ~ (1 | id) + (1 | task) + session * consumption_translation, 
+                                   data = data, 
+                                   REML = FALSE, 
+                                   control = lmerControl(optimizer="bobyqa", 
+                                                         optCtrl = list(maxfun=2e5)),
+                                   method = 'LRT')
+  
+  return(fixed_effects_att)
+  
+}
+
+fixedEffectsAttOriginal <- fixed_effects_att_function(dfOriginal)
+fixedEffectsAttReplication <- fixed_effects_att_function(dfReplication)
+
+# also look at significant fixed effects for conditions which are a direct replication
+# of the original study (control, emission add, rating add)
+fixedEffectsAttDirectReplication <- fixed_effects_att_function(dfReplication %>%
+                                                          filter(consumption_translation == "control" |
+                                                                   consumption_translation == "emission_add" |
+                                                                   consumption_translation == "rating_add"))
 
 ### Set up model -------
 
-contrasts(df$consumption_translation) <- 
-  contr.treatment(levels(df$consumption_translation), base = 1)
+att_model_function <- function(data){
+  
+  # Effect of translation of energy and water consumption (no translation/control) on product choice  
+  contrasts(data$consumption_translation) <- 
+    contr.treatment(levels(data$consumption_translation), base = 1)
+  
+  attention_model <- lmer(diff_t_options ~ (1 | id) + (1| task) + session * consumption_translation, 
+                          data = data, 
+                          REML = FALSE, 
+                          control = lmerControl(optimizer="bobyqa", optCtrl = list(maxfun=2e5)))
+  
+  return(attention_model)
+  
+}
 
-attention_model <- lmer(diff_t_options ~ (1 | id) + (1| task) + session * consumption_translation, 
-                        data = df, 
-                        REML = FALSE, 
-                        control = lmerControl(optimizer="bobyqa", optCtrl = list(maxfun=2e5)))
+attentionModelOriginal <- att_model_function(dfOriginal)
+attentionModelReplication <- att_model_function(dfReplication)
 
-summary(attention_model)
+summary(attentionModelOriginal)
+summary(attentionModelReplication)
 
 
 ### EMM Comparison -------
 
-EMM_att <- emmeans(attention_model, 
-               ~ consumption_translation * session, 
-               type = "response")
+emm_att_function <- function(attention_model){
+  
+  EMM_att <- emmeans(attention_model, 
+                     ~ consumption_translation * session, 
+                     type = "response")
+  
+  emm_session_att <- pairs(EMM_att, reverse = TRUE, simple = "session")
+  
+  emm_session_att_confint <- confint(emm_session_att)
+  
+  # extract estimates
+  results_att <- as.data.frame(summary(emm_session_att_confint))[c('consumption_translation',
+                                                                   'estimate', 
+                                                                   'SE',
+                                                                   'asymp.LCL',
+                                                                   'asymp.UCL')]
+  
+  # round to four decimals
+  results_att[,c('estimate', 
+                 'SE',
+                 'asymp.LCL',
+                 'asymp.UCL')] <- round(results_att[,c('estimate', 
+                                                       'SE',
+                                                       'asymp.LCL',
+                                                       'asymp.UCL')], 4)
+  
+  return(list(
+    emm_session = emm_session_att,
+    emm_session_confint = emm_session_att_confint,
+    results_att = results_att
+  ))
+  
+}
 
-emm_session_att <- pairs(EMM_att, reverse = TRUE, simple = "session")
-
-emm_session_att_confint <- confint(emm_session_att)
-
-
-# extract estimates
-results_att <- as.data.frame(summary(emm_session_att_confint))[c('consumption_translation',
-                                                                  'price_translation',
-                                                                  'estimate', 
-                                                                  'SE',
-                                                                 'asymp.LCL',
-                                                                 'asymp.UCL')]
-
-# round to four decimals
-results_att[,c('estimate', 
-               'SE',
-               'asymp.LCL',
-               'asymp.UCL')] <- round(results_att[,c('estimate', 
-                                               'SE',
-                                               'asymp.LCL',
-                                               'asymp.UCL')], 4)
-
+emmAttOriginal <- emm_att_function(attentionModelOriginal)
+emmAttReplication <- emm_att_function(attentionModelReplication)
 
 
 # Response Times ----
 
 ### Descriptives  ---------
 
-# aggregate data on subject level
-rt_prob_id <- df %>%
-  group_by(session, condition, id) %>%
-  summarise(mean_rt_id = mean(t_decision/1000, na.rm = TRUE), 
-            se_rt_id = sd(t_decision/1000, na.rm = TRUE)/sqrt(sum(!is.na(t_decision/1000)))
-  ) %>%
-  arrange(condition)
+rt_descriptives_function <- function(data){
+  
+  # aggregate data on subject level
+  rt_prob_id <- data %>%
+    group_by(session, condition, id) %>%
+    summarise(mean_rt_id = mean(t_decision/1000, na.rm = TRUE), 
+              se_rt_id = sd(t_decision/1000, na.rm = TRUE)/sqrt(sum(!is.na(t_decision/1000)))
+    ) %>%
+    arrange(condition)
+  
+  # aggregate data on group level
+  rt_prob_group <- rt_prob_id %>%
+    group_by(session, condition) %>%
+    summarise(mean_rt = mean(mean_rt_id, na.rm = TRUE), 
+              se_rt = sd(mean_rt_id, na.rm = TRUE)/sqrt(sum(!is.na(mean_rt_id)))
+    ) %>%
+    arrange(condition)
+  
+  return(list(
+    rt_id_level = rt_prob_id,
+    rt_group_level = rt_prob_group
+  ))
+  
+}
 
-# aggregate data on group level
-rt_prob_group <- rt_prob_id %>%
-  group_by(session, condition) %>%
-  summarise(mean_rt = mean(mean_rt_id, na.rm = TRUE), 
-            se_rt = sd(mean_rt_id, na.rm = TRUE)/sqrt(sum(!is.na(mean_rt_id)))
-  ) %>%
-  arrange(condition)
+rtDescriptivesOriginal <- rt_descriptives_function(dfOriginal)
+rtDescriptivesReplication <- rt_descriptives_function(dfReplication)
 
 
 ### RT Consistency in Session 1 between Conditions ------
 
-# Fit a model without the session interaction
-rt_model_session1 <- glmer(t_decision/1000 ~ consumption_translation + (1 | id) + (1 | task), 
-                               data = df_session1, 
-                               family = "inverse.gaussian"(link='identity'),
-                               control = glmerControl(optimizer="bobyqa", 
-                                                      optCtrl = list(maxfun=2e5)))
+baseline_rt <- function(data){
+  
+  # Subset the data for session 1
+  data_session1 <- subset(data, session == 1)
+  
+  # Fit a model without the session interaction
+  rt_model_session1 <- glmer(t_decision/1000 ~ consumption_translation + (1 | id) + (1 | task), 
+                             data = data_session1, 
+                             family = "inverse.gaussian"(link='identity'),
+                             control = glmerControl(optimizer="bobyqa", 
+                                                    optCtrl = list(maxfun=2e5)))
+  
+  return(rt_model_session1)
+  
+}
 
-# Perform an ANOVA to see if condition has a significant effect
-Anova(rt_model_session1)
+baselineRtOriginal <- baseline_rt(dfOriginal)
+baselineRtReplication <- baseline_rt(dfReplication)
+
+# Perform an ANOVA to see if consumption translation has a significant effect
+Anova(baselineRtOriginal)
+Anova(baselineRtReplication)
 
 
 ### Check for Significant Fixed Effects -------
-fixed_effects_rt <- afex::mixed(t_decision/1000 ~ (session | id) + (1 | task) + session * consumption_translation, 
-                                    data = df, 
-                                    family = "Gamma"(link='identity'), 
-                                    control = glmerControl(optimizer="bobyqa", 
-                                                           optCtrl = list(maxfun=2e5)),
-                                    method = 'LRT')
+
+fixed_effects_rt_function <- function(data){
+  
+  fixed_effects_rt <- afex::mixed(t_decision/1000 ~ (session | id) + (1 | task) + session * consumption_translation, 
+                                  data = data, 
+                                  family = "Gamma"(link='identity'), 
+                                  control = glmerControl(optimizer="bobyqa", 
+                                                         optCtrl = list(maxfun=2e5)),
+                                  method = 'LRT')
+  
+  return(fixed_effects_rt)
+  
+}
+
+fixedEffectsRtOriginal <- fixed_effects_rt_function(dfOriginal)
+fixedEffectsRtReplication <- fixed_effects_rt_function(dfReplication)
+
+# also look at significant fixed effects for conditions which are a direct replication
+# of the original study (control, emission add, rating add)
+fixedEffectsRtDirectReplication <- fixed_effects_rt_function(dfReplication %>%
+                                                          filter(consumption_translation == "control" |
+                                                                   consumption_translation == "emission_add" |
+                                                                   consumption_translation == "rating_add"))
 
 
 ### Set up model ----
 
-# Effect of translation of energy and water consumption (no translation/control) on product choice in absence and presence of a price translation. 
-contrasts(df$consumption_translation) <- 
-  contr.treatment(levels(df$consumption_translation), base = 1)
-rt_model <- glmer(t_decision/1000 ~ (session | id) + (1 | task) + session * consumption_translation, 
-                      data = df, 
-                  family = "Gamma"(link='identity'), 
-                      control = glmerControl(optimizer="bobyqa", 
-                                             optCtrl = list(maxfun=2e5)))
+rt_model_function <- function(data){
+  
+  # Effect of translation of energy and water consumption (no translation/control) on product choice  
+  contrasts(data$consumption_translation) <- 
+    contr.treatment(levels(data$consumption_translation), base = 1)
+  
+  rt_model <- glmer(t_decision/1000 ~ (session | id) + (1 | task) + session * consumption_translation, 
+                    data = data, 
+                    family = "Gamma"(link='identity'), 
+                    control = glmerControl(optimizer="bobyqa", 
+                                           optCtrl = list(maxfun=2e5)))
+  
+  return(rt_model)
+  
+}
 
-summary(rt_model)
+rtModelOriginal <- rt_model_function(dfOriginal)
+rtModelReplication <- rt_model_function(dfReplication)
+
+summary(rtModelOriginal)
+summary(rtModelReplication)
 
 
 ### EMM Comparison -------
 
-###### Price Translation --------
+### EMM Comparison -------
 
-EMM_rt <- emmeans(rt_model, 
-               ~ consumption_translation * session, 
-               type = "response")
-
-emm_session_rt <- pairs(EMM_rt, reverse = TRUE, simple = "session")
-
-emm_session_rt_confint <- confint(emm_session_rt)
-
-
-# extract estimates
-results_rt <- as.data.frame(summary(emm_session_rt_confint))[c('consumption_translation',
+emm_rt_function <- function(rt_model){
+  
+  EMM_rt <- emmeans(rt_model, 
+                    ~ consumption_translation * session, 
+                    type = "response")
+  
+  emm_session_rt <- pairs(EMM_rt, reverse = TRUE, simple = "session")
+  
+  emm_session_rt_confint <- confint(emm_session_rt)
+  
+  # extract estimates
+  results_rt <- as.data.frame(summary(emm_session_rt_confint))[c('consumption_translation',
                                                                  'estimate', 
                                                                  'SE',
                                                                  'asymp.LCL',
                                                                  'asymp.UCL')]
-
-# round to four decimals
-results_rt[,c('estimate', 
-               'SE',
-               'asymp.LCL',
-               'asymp.UCL')] <- round(results_rt[,c('estimate', 
+  
+  # round to four decimals
+  results_rt[,c('estimate', 
+                'SE',
+                'asymp.LCL',
+                'asymp.UCL')] <- round(results_rt[,c('estimate', 
                                                      'SE',
                                                      'asymp.LCL',
                                                      'asymp.UCL')], 4)
+  
+  return(list(
+    emm_session = emm_session_rt,
+    emm_session_confint = emm_session_rt_confint,
+    results_rt = results_rt
+  ))
+  
+}
+
+emmRtOriginal <- emm_rt_function(rtModelOriginal)
+emmRtReplication <- emm_rt_function(rtModelReplication)
+
+
+
+
+
+
+
 
 
 
