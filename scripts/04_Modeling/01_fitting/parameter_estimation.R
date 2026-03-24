2121ce88e16febc273e19bfa8f127d4913bb8386
Oberbauer <bbc6695@uni-hamburg.de>
2026-03-24 15:42:30 +0100

    Run for different group

--- scripts/04_Modeling/01_fitting/parameter_estimation.R
+++ scripts/04_Modeling/01_fitting/parameter_estimation.R
@@ -66,7 +66,7 @@ load("data/preprocessedDataReplication.RData")
 dataset <- "replication"
 # datasets: "original", "replication"
 
-translation_of_interest <- "rating_add"
+translation_of_interest <- "emission_replace"
 # translations for original dataset: "control", "emissions", "operating_costs", "environmental_friendliness"
 # translations for replication dataset: "control", "emission_add", "rating_add", "emission_replace"
 
