d3d9df1a6fb850c6eb156215a1b9039ddbe23f0b
Barbara Oberbauer <barbara.oberbauer@uni-hamburg.de>
2026-03-26 09:15:19 +0100

    Start working on payne index

---
+++ scripts/03_Process_Tracing_Analysis/figurePayneIndex.R
@@ -0,0 +1,53 @@
+#---
+# title: "Computational Mechanisms of Attribute Translations" 
+# author: Barbara Oberbauer (barbara.oberbauer@uni-hamburg.de)
+# purpose: plot dwell time and fixation frequency for attributes
+#---
+
+# Load packages and read data ------
+
+### Clear environment -------
+
+#clear working environment
+rm(list=ls())
+
+#clear all plots
+if(!is.null(dev.list())) dev.off()
+
+### Install packages -------
+
+# List of packages to check and install if necessary
+packages <- c("tidyverse",
+              "dplyr",
+              "ggplot2",
+              "patchwork")
+
+# Function to check if a package is installed
+is_package_installed <- function(package_name) {
+  is.element(package_name, installed.packages()[, "Package"])
+}
+
+# Iterate through the list of packages
+for (package in packages) {
+  if (!is_package_installed(package)) {
+    # Install the package
+    install.packages(package)
+  }
+}
+
+# Load required libraries
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+
+# Load theme
+source("R/theme.R")
+theme_set(themeMEAT())
+
+rm(package, packages, is_package_installed)
+
+
+### Load data ---------
+
+load("data/preprocessedDataReplication.RData")
