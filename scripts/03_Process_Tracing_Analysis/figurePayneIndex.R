cb08f308847a0648d82cf20a55a25e87f19b7b6e
Barbara Oberbauer <barbara.oberbauer@uni-hamburg.de>
2026-03-26 10:50:30 +0100

    Plot Payne Index

--- scripts/03_Process_Tracing_Analysis/figurePayneIndex.R
+++ scripts/03_Process_Tracing_Analysis/figurePayneIndex.R
@@ -51,3 +51,116 @@ rm(package, packages, is_package_installed)
 ### Load data ---------
 
 load("data/preprocessedDataReplication.RData")
+
+# Calculate within alternative and within attribute transitions ------
+
+# dfReplicationProcess$withinAttribute <- NA
+# dfReplicationProcess$withinAlternative <- NA
+# 
+# # within option
+# 
+# for (row in 1:nrow(dfReplicationProcess)) {
+#   
+#   if (dfReplicationProcess$fixNum[row] != 1) {
+#     
+#     # is the fixated option non-eco?
+#     currentOption <- grepl("NonEco", dfReplicationProcess$name[row], fixed = TRUE)
+#     previousOption <- grepl("NonEco", dfReplicationProcess$name[row-1], fixed = TRUE)
+#     
+#     # if both are either true or false, the transition was withinOption
+#     ifelse(currentOption == previousOption, dfReplicationProcess$withinAlternative[row] <- 1,
+#            dfReplicationProcess$withinAlternative[row] <- 0)
+#     
+#   }
+#   
+# }
+# 
+# # within attribute
+# 
+# for (row in 1:nrow(dfReplicationProcess)) {
+#   
+#   if (dfReplicationProcess$fixNum[row] != 1) {
+#     
+#     # is the fixated option non-eco?
+#     currentAttribute <- str_remove(dfReplicationProcess$name[row], "NonEco")
+#     currentAttribute <- str_remove(currentAttribute, "Eco")
+#     
+#     previousAttribute <- str_remove(dfReplicationProcess$name[row-1], "NonEco")
+#     previousAttribute <- str_remove(previousAttribute, "Eco")
+#     
+#     # if both are either true or false, the transition was withinOption
+#     ifelse(currentAttribute == previousAttribute, dfReplicationProcess$withinAttribute[row] <- 1,
+#            dfReplicationProcess$withinAttribute[row] <- 0)
+#     
+#   }
+#   
+# }
+
+# AI suggestion is faster 
+
+dfReplicationProcess <- dfReplicationProcess %>%
+  group_by(id, session, consumption_translation, trial, task) %>%
+  mutate(
+    # extract attribute (remove Eco/NonEco prefix)
+    attribute     = str_remove(name, "NonEco") %>% str_remove("Eco"),
+    isNonEco      = grepl("NonEco", name, fixed = TRUE),
+    
+    # compare to previous fixation within trial
+    withinAlternative = if_else(fixNum == 1, NA, as.integer(isNonEco == lag(isNonEco))),
+    withinAttribute   = if_else(fixNum == 1, NA, as.integer(attribute == lag(attribute)))
+  ) %>%
+  select(-attribute, -isNonEco) %>%  # remove helper columns if not needed
+  ungroup()
+
+# Calculate Payne Index -----
+
+dfReplicationProcessSubset <- dfReplicationProcess %>%
+  filter(fixNum != 1)
+
+PayneIndex <- dfReplicationProcessSubset %>%
+  group_by(id, session, consumption_translation) %>%
+  summarize(PI = (sum(withinAlternative) - sum(withinAttribute))/
+              (sum(withinAlternative) + sum(withinAttribute))
+            )
+
+# Plot Payne Index -----
+
+plotPayneIndex <- 
+ggplot(data = PayneIndex,
+       aes(x = consumption_translation,
+           y = PI,
+           fill = session)) +
+  geom_point(aes(color = session),
+             position = position_jitterdodge(dodge.width = 0.5, 
+                                             jitter.width = 0.2, 
+                                             jitter.height = 0),
+             size = 1.1, alpha = 0.7, show.legend = FALSE) +
+  stat_summary(fun = mean, 
+               geom = "bar",
+               linewidth = 1.2,
+               position = "dodge",
+               width = 0.6, 
+               color = "black") +
+  stat_summary(fun.data = mean_se, 
+               geom = "errorbar", 
+               position = position_dodge(width = 0.6), 
+               width = 0.2, 
+               color = "black",
+               linewidth = 1.2) + 
+  scale_fill_manual(values = scales::alpha(color_sessions, 0.4)) +  
+  scale_color_manual(values = color_sessions) +
+  scale_x_discrete(labels = labelsReplication) +
+  labs(x = "Experimental Condition", 
+       y = "Payne Index",
+       title = "Replication",
+       fill = "Session")
+  
+# Save plot
+ggsave("figures/figurePayneIndex.pdf", 
+       plotPayneIndex, 
+       width = 9,
+       height = 6,
+       units = "in",
+       device = cairo_pdf)
+
+
