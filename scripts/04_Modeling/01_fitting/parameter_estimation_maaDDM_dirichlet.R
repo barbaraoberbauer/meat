25c16f960b4c06e0078c6e24c59f2beee2f6673e
Oberbauer <bbc6695@uni-hamburg.de>
2026-05-19 17:10:42 +0200

    Run for different group

--- scripts/04_Modeling/01_fitting/parameter_estimation_maaDDM_dirichlet.R
+++ scripts/04_Modeling/01_fitting/parameter_estimation_maaDDM_dirichlet.R
@@ -63,10 +63,10 @@ load("data/preprocessedDataReplication.RData")
 
 ### Specify subset of data ----
 
-dataset <- "replication"
+dataset <- "original"
 # datasets: "original", "replication"
 
-translation_of_interest <- "rating_add"
+translation_of_interest <- "control"
 # translations for original dataset: "control", "emissions", "operating_costs", "environmental_friendliness"
 # translations for replication dataset: "control", "emission_add", "rating_add", "emission_replace", "rating_replace"
 
