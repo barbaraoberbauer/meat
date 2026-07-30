8bf6ab622661c123604aecafc33b4e22f8cc8a01
Barbara Oberbauer <barbara.oberbauer@uni-hamburg.de>
2026-07-30 10:25:32 +0200

    Implement reverse fixations

--- R/functions/fun_calculate_sampling_probability.R
+++ R/functions/fun_calculate_sampling_probability.R
@@ -20,16 +20,26 @@
 #'   
 calculate_sampling_prob <- function(data,
                                     attended_var,
+                                    fix_var = fixNum,
                                     valid_combos = NULL,
                                     ci_level = 0.95) {
   
   attended_var <- enquo(attended_var)
   var_name <- as_name(attended_var)
   
+  fix_var <- enquo(fix_var)
+  fix_var_name <- as_name(fix_var)
+  
+  # Work around tidyr::nesting()'s unreliable handling of injected (!!) quosures
+  # by renaming the fixation-index column to a fixed literal name up front,
+  # then renaming it back to its original name at the end.
+  data <- data %>%
+    rename(.fix_idx = !!fix_var)
+  
   subj_prob <- data %>%
-    count(id, session, consumption_translation, fixNum, !!attended_var, name = "n_fix") %>%
+    count(id, session, consumption_translation, .fix_idx, !!attended_var, name = "n_fix") %>%
     complete(
-      nesting(id, session, consumption_translation, fixNum),
+      nesting(id, session, consumption_translation, .fix_idx),
       !!attended_var,
       fill = list(n_fix = 0)
     )
@@ -40,14 +50,14 @@ calculate_sampling_prob <- function(data,
   }
   
   subj_prob <- subj_prob %>%
-    group_by(id, session, consumption_translation, fixNum) %>%
+    group_by(id, session, consumption_translation, .fix_idx) %>%
     mutate(prob_fix = n_fix / sum(n_fix)) %>%
     ungroup()
   
   crit <- 1 - (1 - ci_level) / 2
   
   group_prob <- subj_prob %>%
-    group_by(session, consumption_translation, fixNum, !!attended_var) %>%
+    group_by(session, consumption_translation, .fix_idx, !!attended_var) %>%
     summarize(
       mean_prob = mean(prob_fix, na.rm = TRUE),
       sd_prob   = sd(prob_fix, na.rm = TRUE),
@@ -59,6 +69,11 @@ calculate_sampling_prob <- function(data,
       ci_lower = ifelse(n >= 2, mean_prob - qt(crit, df = pmax(n - 1, 1)) * se_prob, NA_real_),
       ci_upper = ifelse(n >= 2, mean_prob + qt(crit, df = pmax(n - 1, 1)) * se_prob, NA_real_)
     )
+  
+  # rename the fixation-index column back to whatever was passed as fix_var
+  subj_prob <- subj_prob %>% rename(!!fix_var_name := .fix_idx)
+  group_prob <- group_prob %>% rename(!!fix_var_name := .fix_idx)
+  
   
   list(subject = subj_prob, group = group_prob)
 }
